7cdf8e2c3afb89e21d5d87cf62ac28d11a9618a8
Nathan Leiby <nathan.leiby@clever.com>
2019-09-21 22:59:20 -0700

    birthday

---
+++ index.ts
@@ -0,0 +1,34 @@
+import * as puppeteer from "puppeteer";
+import * as fs from "fs";
+
+(async () => {
+  const items = new Set();
+
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://www.cotopaxi.com/products/taal-convertible-tote");
+
+  const numReloads = 25;
+  for (let i = 0; i < numReloads; i++) {
+    const images = await page.evaluate(() => {
+      return Array.from(
+        document.querySelectorAll("figure.choice-image:not(.soldout) img")
+      ).map(x => x.outerHTML);
+    });
+
+    images.forEach(g => items.add(g));
+    console.log(`Set has ${items.size} items`);
+
+    await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+  }
+
+  const output = Array.from(items).join("\n");
+
+  console.log("writing output");
+  fs.writeFileSync(`images-${+new Date()}.html`, output);
+
+  await browser.close();
+})();
